5c25f1e83d01231ae00faf6bdd968583a06284a3
go-green1x <gogreen1x@tutanota.com>
2023-03-18 17:38:07 +0500

    small bug fix

--- userAuth/views.py
+++ userAuth/views.py
@@ -12,6 +12,7 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import UserSerializer, ChangePasswordSerializer, ModelProfileSerializer, UserDetails
+from .models import Profile
 # Create your views here.
 
 class UserCreateView(generics.CreateAPIView):
